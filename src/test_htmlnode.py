b2291fc786a3a0dfd8386df2c1ebe595b9baba04
Marcel <marcel.ba@proton.me>
2025-08-13 20:38:40 +0200

    ich habe noch ein paar tests hinzugefügt

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -49,10 +49,29 @@ class TestHTMLNode(unittest.TestCase):
         grandchild_node = LeafNode("b", "grandchild")
         child_node = ParentNode("span", [grandchild_node])
         parent_node = ParentNode("div", [child_node])
-        self.assertEqual(
-            parent_node.to_html(),
-            "<div><span><b>grandchild</b></span></div>",
-        )
+        self.assertEqual(parent_node.to_html(), "<div><span><b>grandchild</b></span></div>")
+
+    def test_to_html_with_no_children(self):
+        node = ParentNode("p", children=None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_to_html_with_no_tag(self):
+        child_node = LeafNode("p", "child")
+        parent_node = ParentNode(None, [child_node])
+        with self.assertRaises(ValueError):
+            parent_node.to_html()
+
+    def test_to_html_with_bad_child(self):
+        child_node = LeafNode("span", None)
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div></div>")
+
+    def test_to_html_with_bad_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode(None, [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div></div>")
 
 
 if __name__ == "__main__":
