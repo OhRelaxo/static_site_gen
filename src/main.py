7490e70229f30bce9fab325a8889b8bb1d003ace
Marcel <marcel@aurora.fritz.box>
2025-08-14 18:59:07 +0200

    weiter machen

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
-from block_markdown import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type
 
 def main():
-    print(markdown_to_blocks("""# This is a heading
+    text_blocks = markdown_to_blocks("""# This is a heading
 
 This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
+This is still just a paragraph of text.
 
 - This is the first list item in a list block
 - This is a list item
-- This is another list item"""))
+- This is another list item""")
+    print(block_to_block_type(text_blocks[1]))
 main()
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlcokType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     new_markdown = markdown.split("\n\n")
     clean_markdown = []
@@ -6,3 +16,17 @@ def markdown_to_blocks(markdown):
             continue
         clean_markdown.append(text.strip())
     return clean_markdown
+
+def block_to_block_type(markdown_block: str):
+    if not markdown_block:
+        return None
+    block_list = markdown_block.split(" ")
+    split_test = markdown_block.split("\n")
+    print(split_test.split(" "))
+    first_chars = block_list[0]
+    last_chars = block_list[-1]
+    if "#" in first_chars:
+        return BlcokType.HEADING
+    if "```" in first_chars and "```" in last_chars:
+        return BlcokType.CODE
+
