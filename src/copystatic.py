a29b92516ac538d3fa5c4cefa93e6dc02295cc88
Marcel <marcel.ba@proton.me>
2025-08-15 03:50:17 +0200

    added copy function

---
+++ src/copystatic.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+import logging
+
+logging.basicConfig(
+    filename='copy_log.txt',
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+def copy_content():
+    working_dir = os.getcwd()
+    public_dir = os.path.join(working_dir, "public")
+    if os.path.exists(public_dir):
+        shutil.rmtree(public_dir)
+        logging.info(f"deleted directory: {public_dir}")
+    os.mkdir(public_dir)
+    logging.info(f"created directory: {public_dir}")
+    static_dir = os.path.join(working_dir, "static")
+    if os.path.exists(static_dir):
+        logging.info("starting with copy process:")
+        copy_content_helper(static_dir, public_dir)
+        logging.info("copy process was successful")
+    else:
+        raise NotADirectoryError(static_dir)
+
+def copy_content_helper(src_path, dst_path):
+    files = os.listdir(src_path)
+    for file in files:
+        if os.path.isdir(os.path.join(src_path, file)):
+            new_dst_path = os.path.join(dst_path, file)
+            new_src_path = os.path.join(src_path, file)
+            if not os.path.isfile(new_dst_path):
+                os.mkdir(new_dst_path)
+                logging.info(f"created directory: {new_dst_path}")
+            copy_content_helper(new_src_path, new_dst_path)
+        else:
+            shutil.copy(os.path.join(src_path, file), dst_path)
+            logging.info(f"copied: {src_path} to {dst_path}")
--- src/main.py
+++ src/main.py
@@ -1,16 +1,7 @@
-from markdown_to_html import markdown_to_html_node
+from copystatic import copy_content
+
 
 def main():
-    md = """
-This is **bolded** paragraph
-text in a p
-tag here
 
-This is another paragraph with _italic_ text and `code` here
-
-    """
-
-    node = markdown_to_html_node(md)
-    md2 = """# This is a test"""
-    print(node.to_html())
+    print(copy_content())
 main()
